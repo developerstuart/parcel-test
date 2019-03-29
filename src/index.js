c9f64ac613e6193d5c198387143f7e9364e7b6cf
Stuart Lawrence <boffolow@gmail.com>
2019-03-29 11:22:57 +0000

    Added text line

--- src/index.js
+++ src/index.js
@@ -7,4 +7,5 @@ document.getElementById("app").innerHTML = `
   We use Parcel to bundle this sandbox, you can find more info about Parcel
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
+This is amazing
 `;
